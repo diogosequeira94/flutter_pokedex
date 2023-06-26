64be71829a95b8361ea27ed4dce157c84336bed8
DiogoSequeira <diogosequeira@hotmail.fr>
2023-06-26 16:03:39 +0100

    add repository tests

---
+++ repositories/pokemon/test/pokemon_repository_test.dart
@@ -0,0 +1,71 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:pokemon/pokemon.dart';
+import 'package:pokemon/src/api/models/species/habitat.dart';
+import 'package:pokemon/src/api/models/species/species_response.dart';
+import 'package:test/test.dart';
+
+class MockPokemonApiClient extends Mock implements PokemonApiClient {}
+
+void main() {
+  late PokemonRepository pokemonRepository;
+  late PokemonApiClient pokemonApiClient;
+
+  setUp(() {
+    pokemonApiClient = MockPokemonApiClient();
+    pokemonRepository = PokemonRepository.testing(pokemonApiClient);
+  });
+
+  group('PokemonRepository', () {
+    test('getFirstPokemonGen is called and returns PokemonGenerationResponse',
+        () async {
+      const pokemonGenerationResponse =
+          PokemonGenerationResponse(count: 151, pokemonItems: []);
+      when(() => pokemonApiClient.getFirstPokemonGen()).thenAnswer(
+        (_) => Future.value(pokemonGenerationResponse),
+      );
+      final response = await pokemonRepository.getFirstPokemonGen();
+      expect(response, pokemonGenerationResponse);
+    });
+
+    test('getPokemonByName is called and returns Pokemon', () async {
+      const pokemon = Pokemon(
+        height: 2,
+        weight: 2,
+        abilities: [],
+        moves: [],
+        types: [],
+        stats: [],
+      );
+
+      when(() => pokemonApiClient.getPokemonByName('Pikachu')).thenAnswer(
+        (_) => Future.value(pokemon),
+      );
+
+      final response =
+          await pokemonRepository.getPokemonByName(name: 'Pikachu');
+
+      expect(response, pokemon);
+    });
+
+    test('getSpeciesInformation is called and returns SpeciesResponse',
+        () async {
+      const speciesResponse = SpeciesResponse(
+        eggGroups: [],
+        flavorTextEntries: [],
+        habitat: Habitat(name: 'Mountain'),
+        isBaby: false,
+        isLegendary: false,
+        isMythical: false,
+      );
+      when(() => pokemonApiClient.getSpeciesInformation('Charmander'))
+          .thenAnswer(
+        (_) => Future.value(speciesResponse),
+      );
+
+      final response =
+          await pokemonRepository.getSpeciesInformation(name: 'Charmander');
+
+      expect(response, speciesResponse);
+    });
+  });
+}
--- repositories/pokemon/test/pokemon_test.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:pokemon/pokemon.dart';
-
-void main() {}
